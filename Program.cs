f280e9075b2eec724ba67f2c1d01ce3a008a08b7
Gnanaprasuna <ittamgnanaprasuna1755@gmail.com>
2024-01-04 14:20:03 +0530

    Successfully Added

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student_Data
+{
+    internal class Program
+    {
+            private static void FileFun()
+
+            {
+                string path = @"C:\Users\ittam\OneDrive\Desktop\C#_Projects\Project2\StudentDetails";
+
+                if (File.Exists(path))
+                {
+                    using (StreamReader sr = File.OpenText(path))
+                    {
+                        string tillEnd = sr.ReadToEnd();
+                        Console.WriteLine("File Exists, reading in:\n" + tillEnd);
+                    }
+                }
+                else
+                {
+                    using (StreamWriter sw = File.CreateText(path))
+                    {
+                        sw.WriteLine("StudentData\nName\nRollnumber\nSubject");
+                        Console.WriteLine("File doesnt exist.");
+                    }
+                }
+            }
+            private static void Main(string[] args)
+            {
+                FileFun();
+            }
+        }
+    }
+
+
